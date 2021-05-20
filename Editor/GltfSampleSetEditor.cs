8e0fcd9b9457eda4315cc3f1c4341d1bbdff708e
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-05-21 01:35:18 +0200

    fix: compiles again

--- Editor/GltfSampleSetEditor.cs
+++ Editor/GltfSampleSetEditor.cs
@@ -22,8 +22,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.AccessControl;
 using GLTFast;
-using GLTFast.Schema;
-using GLTFast.Tests;
 using UnityEditor.SceneManagement;
 #endif
 
